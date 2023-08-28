c6c6ffc9d723b772c75c381a06be6718da1928d2
Alexander Bulatov <bulatovdrive@yandex.ru>
2023-08-28 22:06:22 +0300

    14-th, 5-st commit

--- app.js
+++ app.js
@@ -36,6 +36,8 @@ app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }),
 }), login);
 
